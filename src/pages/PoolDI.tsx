eca13db425d7533ac871a2b0f68e2b744d85ba29
Nick Novoa <nicknovoa03@gmail.com>
2023-09-21 13:35:32 -0500

    remove DI and Prestige

--- src/pages/PoolDI.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { PoolDI } from '../views';
-const IndexPage = () => {
-  return <PoolDI />;
-};
-
-export default IndexPage;
--- src/pages/PoolPrestige.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { PoolPrestige } from '../views';
-const IndexPage = () => {
-  return <PoolPrestige />;
-};
-
-export default IndexPage;
