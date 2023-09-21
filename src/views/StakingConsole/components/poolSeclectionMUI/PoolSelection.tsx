c8b1972d84cead239d77e3082ffab8d256090671
Nick Novoa <nicknovoa03@gmail.com>
2023-09-21 12:23:26 -0500

    update pool selection content

--- src/views/StakingConsole/components/poolSeclectionMUI/PoolSelection.tsx
+++ src/views/StakingConsole/components/poolSeclectionMUI/PoolSelection.tsx
@@ -16,69 +16,29 @@ import { ERC20BalanceOf, ERC721BalanceOf } from '../../../../components/contract
 import { useAccount } from 'wagmi';
 import getNFTMetadata from '../../../../components/nfts/NFTMetadata';
 import { nftMetadataDictionary } from '../../../../components/nftData/nftMetadataDictionary';
+import Image from 'next/image';
+import WarholImage from '../../../../../public/NFT-image.png';
 
 const pools = [
   {
-    title: 'Pool 1',
-    features: [
-      'THRESHOLD: 10,000 iAI Tokens',
-      'Minimum 9022 NFTs Required: 1',
-      'YEARLY Distribution on iAI THRESHOLD: 2%'
-    ],
-    iAiTokenReqs: 10,
-    nftCountReqs: 1,
+    title: 'Fractional Warhol Mint',
+    background: WarholImage,
     size: 4,
     href: '/Pool1'
   },
   {
-    title: 'Pool 2',
-    features: [
-      'THRESHOLD: 30,000 iAI Tokens',
-      'Minimum 9022 NFTs Required: 2',
-      'YEARLY Distribution on iAI THRESHOLD: 4%'
-    ],
-    iAiTokenReqs: 30000,
-    nftCountReqs: 1,
+    title: 'Physical Warhol Mint',
+    background: WarholImage,
     size: 4,
     href: '/Pool2'
   },
   {
-    title: 'Pool 3',
-    features: [
-      'THRESHOLD: 100,000 iAI Tokens',
-      'Minimum 9022 NFTs Required: 3-10',
-      'YEARLY Distribution on iAI THRESHOLD: 5.5%',
-      '.5% will be added to the total distribution for each NFT held >3. Max 9% iAI THRESHOLD'
-    ],
-    iAiTokenReqs: 100000,
-    nftCountReqs: 3,
+    title: 'AI Companion Bot',
+    background: WarholImage,
     size: 4,
     href: '/Pool3'
   }
 ];
-const prestigePool = {
-  title: 'Prestige Ambassador',
-  features: [
-    'THRESHOLD: 200,000 iAI Tokens',
-    '9022 NFTs Required: Prestige',
-    'YEARLY Distribution on iAI THRESHOLD: 10%'
-  ],
-  iAiTokenReqs: 200000,
-  size: 6,
-  href: '/PoolPrestige'
-};
-
-const PoolDI = {
-  title: 'Destination Inheritance Ambassador',
-  features: [
-    'THRESHOLD: 300,000 iAI Tokens',
-    '9022 NFTs Required: Destination Inheritance',
-    'YEARLY Distribution on iAI THRESHOLD: 12%'
-  ],
-  iAiTokenReqs: 300000,
-  size: 6,
-  href: '/PoolDI'
-};
 
 const PoolSelection = () => {
   const theme = useTheme();
@@ -185,77 +145,20 @@ const PoolSelection = () => {
             sx={{ borderRadius: 3, border: '1px solid white' }}
             data-aos={'flip-left'}
           >
-            <CardContent
-              sx={{
-                padding: 4
-              }}
-            >
-              <Box marginBottom={0}>
-                <Typography variant={'h4'} align="center" color={theme.palette.common.white}>
+            <CardContent>
+              <Box marginBottom={3}>
+                <Typography variant={'h4'} align="center" color={theme.palette.common.white} marginBottom={2}>
                   <Box component={'span'} fontWeight={600}>
                     {item.title}
                   </Box>
                 </Typography>
               </Box>
-
-              <Box marginBottom={2}>
-                <Typography variant={'h5'} align="center" color={theme.palette.common.white}>
-                  {Number(NFTBalanceAmount) >= item.nftCountReqs &&
-                  Number(ethers.utils.formatEther(iAIbalanceAmount)) >= item.iAiTokenReqs ? (
-                    <Box
-                      component="span"
-                      sx={{
-                        padding: 0.5,
-                        color: 'green'
-                      }}
-                      fontWeight={600}
-                    >
-                      Eligible
-                    </Box>
-                  ) : (
-                    <Box
-                      component="span"
-                      sx={{
-                        padding: 0.5,
-                        color: 'red'
-                      }}
-                      fontWeight={600}
-                    >
-                      ineligible
-                    </Box>
-                  )}
-                </Typography>
+              <Box borderRadius={5} margin={2} maxWidth={{ md: 350 }} data-aos={'zoom-in'}>
+                <Image alt="Background Image" src={item.background} quality={50} />
               </Box>
-              <Grid container spacing={1}>
-                {item.features.map((feature, j) => (
-                  <Grid item xs={12} key={j}>
-                    <Box component={ListItem} padding={0}>
-                      <Box component={ListItemAvatar} minWidth={'auto !important'} marginRight={2}>
-                        <Box component={Avatar} bgcolor={theme.palette.primary.main} width={20} height={20}>
-                          <svg
-                            width={12}
-                            height={12}
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </Box>
-                      </Box>
-                      <ListItemText primary={feature} style={{ color: theme.palette.common.white }} />
-                    </Box>
-                  </Grid>
-                ))}
-              </Grid>
             </CardContent>
             <Box flexGrow={1} />
-
-            <CardActions sx={{ justifyContent: 'flex-end', padding: 4 }}>
+            <CardActions sx={{ justifyContent: 'center', marginBottom: 3, marginTop: -2 }}>
               <Button size={'large'} variant={'contained'} href={item.href}>
                 Learn More
               </Button>
@@ -263,180 +166,6 @@ const PoolSelection = () => {
           </Box>
         </Grid>
       ))}
-
-      <Grid item md={prestigePool.size}>
-        <Box
-          component={Card}
-          height={1}
-          display={'flex'}
-          flexDirection={'column'}
-          bgcolor={theme.palette.common.black}
-          sx={{ borderRadius: 3, border: '1px solid white' }}
-          data-aos={'flip-left'}
-        >
-          <CardContent
-            sx={{
-              padding: 4
-            }}
-          >
-            <Box marginBottom={0}>
-              <Typography variant={'h4'} align="center" color={theme.palette.common.white}>
-                <Box component={'span'} fontWeight={600}>
-                  {prestigePool.title}
-                </Box>
-              </Typography>
-            </Box>
-
-            <Box marginBottom={2}>
-              <Typography variant={'h5'} align="center" color={theme.palette.common.white}>
-                {prestigeFlag && Number(ethers.utils.formatEther(iAIbalanceAmount)) >= prestigePool.iAiTokenReqs ? (
-                  <Box
-                    component="span"
-                    sx={{
-                      padding: 0.5,
-                      color: 'green'
-                    }}
-                    fontWeight={600}
-                  >
-                    Eligible
-                  </Box>
-                ) : (
-                  <Box
-                    component="span"
-                    sx={{
-                      padding: 0.5,
-                      color: 'red'
-                    }}
-                    fontWeight={600}
-                  >
-                    ineligible
-                  </Box>
-                )}
-              </Typography>
-            </Box>
-            <Grid container spacing={1}>
-              {prestigePool.features.map((feature, j) => (
-                <Grid item xs={12} key={j}>
-                  <Box component={ListItem} padding={0}>
-                    <Box component={ListItemAvatar} minWidth={'auto !important'} marginRight={2}>
-                      <Box component={Avatar} bgcolor={theme.palette.primary.main} width={20} height={20}>
-                        <svg
-                          width={12}
-                          height={12}
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </Box>
-                    </Box>
-                    <ListItemText primary={feature} style={{ color: theme.palette.common.white }} />
-                  </Box>
-                </Grid>
-              ))}
-            </Grid>
-          </CardContent>
-          <Box flexGrow={1} />
-
-          <CardActions sx={{ justifyContent: 'flex-end', padding: 4 }}>
-            <Button size={'large'} variant={'contained'} href={prestigePool.href}>
-              Learn More
-            </Button>
-          </CardActions>
-        </Box>
-      </Grid>
-
-      <Grid item md={PoolDI.size}>
-        <Box
-          component={Card}
-          height={1}
-          display={'flex'}
-          flexDirection={'column'}
-          bgcolor={theme.palette.common.black}
-          sx={{ borderRadius: 3, border: '1px solid white' }}
-          data-aos={'flip-left'}
-        >
-          <CardContent
-            sx={{
-              padding: 4
-            }}
-          >
-            <Box marginBottom={0}>
-              <Typography variant={'h4'} align="center" color={theme.palette.common.white}>
-                <Box component={'span'} fontWeight={600}>
-                  {PoolDI.title}
-                </Box>
-              </Typography>
-            </Box>
-
-            <Box marginBottom={2}>
-              <Typography variant={'h5'} align="center" color={theme.palette.common.white}>
-                {destinationInheritanceFlag &&
-                Number(ethers.utils.formatEther(iAIbalanceAmount)) >= PoolDI.iAiTokenReqs ? (
-                  <Box
-                    component="span"
-                    sx={{
-                      padding: 0.5,
-                      color: 'green'
-                    }}
-                    fontWeight={600}
-                  >
-                    Eligible
-                  </Box>
-                ) : (
-                  <Box
-                    component="span"
-                    sx={{
-                      padding: 0.5,
-                      color: 'red'
-                    }}
-                    fontWeight={600}
-                  >
-                    ineligible
-                  </Box>
-                )}
-              </Typography>
-            </Box>
-            <Grid container spacing={1}>
-              {PoolDI.features.map((feature, j) => (
-                <Grid item xs={12} key={j}>
-                  <Box component={ListItem} padding={0}>
-                    <Box component={ListItemAvatar} minWidth={'auto !important'} marginRight={2}>
-                      <Box component={Avatar} bgcolor={theme.palette.primary.main} width={20} height={20}>
-                        <svg
-                          width={12}
-                          height={12}
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </Box>
-                    </Box>
-                    <ListItemText primary={feature} style={{ color: theme.palette.common.white }} />
-                  </Box>
-                </Grid>
-              ))}
-            </Grid>
-          </CardContent>
-          <Box flexGrow={1} />
-          <CardActions sx={{ justifyContent: 'flex-end', padding: 4 }}>
-            <Button size={'large'} variant={'contained'} href={PoolDI.href}>
-              Learn More
-            </Button>
-          </CardActions>
-        </Box>
-      </Grid>
     </Grid>
   );
 };
